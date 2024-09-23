c3b32fac9acfd2e788ddd381687c669346f2ecdc
Home WSL Diogo <hiperdg.gm@gmail.com>
2024-09-24 00:53:24 +0100

    db now created if doesnt exists

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -6,13 +6,23 @@ mysqld --user=root --socket=/run/mysqld/mysqld.sock --datadir='/var/lib/mysql' &
 sleep 5
 
 echo "------------------- ROOT PASSWORD AND USER -------------------"
+
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';" -p"$MARIADB_ROOT_PASSWORD"
+echo "Root password changed"
+
 mysql -u root -e "ALTER USER '$MARIADB_USER'@'localhost' IDENTIFIED BY '$MARIADB_PASSWORD';" -p"$MARIADB_ROOT_PASSWORD"
+echo "User password changed"
 
 echo "------------------- SETUP DB -------------------"
 
-# mysql -u root -e "CREATE DATABASE $MARIADB_DATABASE;" -p"$MARIADB_ROOT_PASSWORD"
-echo "CREATE DATABASE $MARIADB_DATABASE ;" | mysql -u root -p"$MARIADB_ROOT_PASSWORD"
+DB_EXISTS=$(mysql -u root -p"$MARIADB_ROOT_PASSWORD" -e "SELECT SCHEMA_NAME FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = '$MARIADB_DATABASE';")
+
+if [ -z "$DB_EXISTS" ]; then
+  echo "Database $MARIADB_DATABASE does not exist. Creating..."
+  mysql -u root -p"$MARIADB_ROOT_PASSWORD" -e "CREATE DATABASE \`$MARIADB_DATABASE\`;"
+else
+  echo "Database $MARIADB_DATABASE already exists."
+fi
 
 echo "------------------- DONE SETUP DB -------------------"
 
