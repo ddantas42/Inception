ebbdbea77ce65c9880b226e3e6c60bcde2d611c0
Home WSL Diogo <hiperdg.gm@gmail.com>
2024-10-01 17:59:09 +0100

    fixed env var

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -6,7 +6,7 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 	wp cli update --yes --allow-root
 	wp core download --allow-root
 	wp config create --dbname=${MARIADB_DATABASE} --dbuser=${MARIADB_USER} --dbpass=${MARIADB_PASSWORD} --dbhost=mariadb --allow-root --path=/var/www/html
-	wp core install --url=${DOMAIN_NAME} --title=title --admin_user=${WODPRESS_ADMIN} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WODPRESS_ADMIN_EMAIL} --allow-root
+	wp core install --url=${DOMAIN_NAME} --title=title --admin_user=${WORDPRESS_ADMIN} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL} --allow-root
 	wp theme install teluro --activate --allow-root
 	wp user create "${WORDPRESS_USER}" "${WORDPRESS_USER_EMAIL}" --role=author --user_pass=${WORDPRESS_USER_PASSWORD} --display_name="${MARIADB_USER}" --allow-root
 else
