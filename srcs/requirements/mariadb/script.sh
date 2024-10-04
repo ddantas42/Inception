ede71c650d4b9b04f112d3a6f9cef578804c8c29
Your Name <you@example.com>
2024-10-04 16:01:27 +0100

    mariadb update

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -9,8 +9,12 @@ if [ -z "$(ls -A /var/lib/mysql)" ]; then
   sleep 5
 
   echo "done"
-  
-  # mysqladmin -u root -p"$MARIADB_ROOT_PASSWORD" shutdown
+
+  service mariadb stop
+
+fi
+
+
 mysqld --user=root --socket=/run/mysqld/mysqld.sock --datadir='/var/lib/mysql' &
 
 
@@ -29,7 +33,6 @@ echo "Root password changed"
 
 mysql -u root -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';" -p"$MARIADB_ROOT_PASSWORD"
 
-# mysql -u root -e "ALTER USER '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';" -p"$MARIADB_ROOT_PASSWORD"
 echo "User password changed"
 
 
@@ -52,10 +55,6 @@ echo "------------------- DONE SETUP DB -------------------"
 echo "------------------- Restarting DB -------------------"
 
 mysqladmin -u root -p"$MARIADB_ROOT_PASSWORD" shutdown
-  service mariadb stop
-
-fi
-
 
 
 exec mysqld --user=root --socket=/run/mysqld/mysqld.sock --datadir='/var/lib/mysql'
